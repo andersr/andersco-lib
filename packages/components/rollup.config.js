6cc552e1952a1dbd561b83debeb886083b0cc2b2
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2022-08-22 06:50:12 -0400

    chore(components): Rm unused rollup configs

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -2,21 +2,13 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-// import del from "rollup-plugin-delete";
 
 const packageJson = require("./package.json");
-// const playgroundComponents = "playground/src/generated-components";
 
 export default [
   {
     input: "src/index.ts",
-    // external: ["styled-components"],
     output: [
-      // {
-      //   file: playgroundComponents + "/index.js",
-      //   format: "esm",
-      //   banner: "/* eslint-disable */",
-      // },
       {
         file: packageJson.main,
         format: "cjs",
@@ -30,10 +22,8 @@ export default [
     ],
     plugins: [
       resolve(),
-      // del({ targets: ["dist/*", playgroundComponents] }),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      // postcss(),
     ],
   },
   {
